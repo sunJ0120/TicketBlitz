83467e31104435f6445d0762d28f6a907f445fc9
sunJ0120 <sspure1214@gmail.com>
2026-01-19 17:20:49 +0900

    feat: 웹소켓 에러 코드 분리

---
+++ src/main/java/com/example/be/websocket/exception/WebSocketAuthException.java
@@ -0,0 +1,7 @@
+package com.example.be.websocket.exception;
+
+public class WebSocketAuthException extends RuntimeException {
+  public WebSocketAuthException(String message) {
+    super(message);
+  }
+}

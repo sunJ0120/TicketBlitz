726eb66b98ec4b4db7614fea09dc70cca8793f45
sunJ0120 <sspure1214@gmail.com>
2026-01-21 19:55:27 +0900

    fix: AuthException return 하도록 수정

--- src/main/java/com/example/be/auth/validator/AuthValidator.java
+++ src/main/java/com/example/be/auth/validator/AuthValidator.java
@@ -1,5 +1,7 @@
 package com.example.be.auth.validator;
 
+import com.example.be.auth.exception.AuthErrorCode;
+import com.example.be.auth.exception.AuthException;
 import com.example.be.security.jwt.JwtProvider;
 import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +15,7 @@ public class AuthValidator {
 
   public String validateAndGetToken(String token) throws JwtException {
     if (token == null || token.isBlank()) {
-      throw new IllegalArgumentException("토큰이 존재하지 않거나 빈 문자열입니다.");
+      throw new AuthException(AuthErrorCode.TOKEN_INVALID);
     }
 
     jwtProvider.validateToken(token);

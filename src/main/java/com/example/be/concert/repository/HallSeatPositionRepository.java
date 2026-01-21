fdf87d83689e357ea6e286755b29029e2a2cfbf3
sunJ0120 <sspure1214@gmail.com>
2026-01-21 18:15:42 +0900

    fix: dataInitialization에서 편하게 사용하기 위해 optional로 감쌈

--- src/main/java/com/example/be/concert/repository/HallSeatPositionRepository.java
+++ src/main/java/com/example/be/concert/repository/HallSeatPositionRepository.java
@@ -2,9 +2,10 @@ package com.example.be.concert.repository;
 
 import com.example.be.concert.domain.HallSeatPosition;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HallSeatPositionRepository extends JpaRepository<HallSeatPosition, Long> {
 
-  List<HallSeatPosition> findByHallTemplateId(Long id);
+  Optional<List<HallSeatPosition>> findByHallTemplateId(Long id);
 }
